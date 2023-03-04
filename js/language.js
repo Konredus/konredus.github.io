ab225923b20c5c5ec3924c58aee83797d2cdf376
Andi <andi_haller@hotmail.com>
2023-03-04 19:08:20 -0300

    ES EN translation, localstorage of theme and lang option, white hue

---
+++ js/language.js
@@ -0,0 +1,118 @@
+const lang = {
+  "nav-btn-about": {
+    en: "About me",
+    es: "Sobre mi",
+  },
+  "nav-btn-projects": {
+    en: "Projects",
+    es: "Proyectos",
+  },
+  "nav-btn-connect": {
+    en: "Connect",
+    es: "Conectar",
+  },
+  "presentation-h1-hi": {
+    en: 'Hi! <i id="nav-button-icon" class="bx bxs-hand bx-lg"></i>',
+    es: 'Hola! <i id="nav-button-icon" class="bx bxs-hand bx-lg"></i>',
+  },
+  "presentation-h2-name": {
+    en: "I'm Konrad",
+    es: "Soy Konrad",
+  },
+  "presentation-h3-business": {
+    en: "Business & Data Analyst",
+    es: "Analista de Negocios y Datos",
+  },
+  about: {
+    en: '<i id="nav-button-icon" class="bx bxs-hand-right bx-md"></i> A little about me',
+    es: '<i id="nav-button-icon" class="bx bxs-hand-right bx-md"></i> Un poco sobre mi',
+  },
+  "about-text-1": {
+    en: "Hi, I'm Konrad, an electronic engineer with experience in R&D and repair of industrial machinery. Over the past 6 years I have created 3 ventures, one of which was successful, ranging from sales of Arduino supplies and educational robotics kits, to training students in robotics and programming, and most recently, online adult education in electronics and programming.",
+    es: "Hola, soy Konrad, un ingeniero electrónico con experiencia en investigación y desarrollo, así como en reparación de maquinaria industrial. Durante los últimos 6 años, he creado 3 empresas, una de las cuales tuvo éxito, que van desde la venta de suministros de Arduino y kits educativos de robótica, hasta la formación de estudiantes en robótica y programación, y más recientemente, educación en línea para adultos en electrónica y programación.",
+  },
+  "about-text-2": {
+    en: "These experiences have honed my leadership and business skills, including metrics tracking, communication, and fundraising, and have led me to pursue Business Analytics.",
+    es: "Estas experiencias han perfeccionado mis habilidades de liderazgo y negocios, incluyendo el seguimiento de métricas, la comunicación y la recaudación de fondos, y me han llevado a buscar la especialidad de Análisis de Negocios.",
+  },
+  "about-text-3": {
+    en: "After completing Google's Data Analytics course, I'm now studying Business Analytics at Coder House, and I'm excited to share my insights and help other companies grow with data-driven decisions.",
+    es: "Después de completar el curso de Análisis de Datos de Google, actualmente estoy estudiando Análisis de Negocios en Coder House, y estoy emocionado de compartir mis conocimientos y ayudar a otras empresas a crecer con decisiones basadas en datos.",
+  },
+  "myjourny-title": {
+    en: "<i class='bx bx-walk'></i> From Engineer to Business Analytics",
+    es: "<i class='bx bx-walk'></i> De Ingeniero a Analista de Negocios",
+  },
+  "myjourney-text-1": {
+    en: '<span class="resaltar">- 2008 -</span> Everyone has a story and this is the one that makes the most sense to the 2023 version of me. I began my career working in a remote technical support company in Argentina for Germany, where I gained experience in troubleshooting and managing networks remotely. After this, I worked in research and development for a telecommunications company, where I honed my skills in math, physics, and processes.',
+    es: '<span class="resaltar">- 2008 -</span> Todos tienen una historia y esta es la que tiene más sentido para la versión de mí en el 2023. Comencé mi carrera trabajando en una empresa de soporte técnico remoto en Argentina para Alemania, donde adquirí experiencia en resolución de problemas y gestión de redes de forma remota. Después de eso, trabajé en investigación y desarrollo para una empresa de telecomunicaciones, donde perfeccioné mis habilidades en matemáticas, física y procesos.',
+  },
+  "myjourney-text-2": {
+    en: '<span class="resaltar">- 2015 -</span> In the past seven years, I\'ve had three different entrepreneurial ventures. The first was a sales venture for Arduino supplies, robotics, and educational kits. The second was focused on providing robotics education to students in schools, and the third was an online education platform that provided training in electronics and programming to adults.',
+    es: '<span class="resaltar">- 2015 -</span> En los últimos siete años, he tenido tres emprendimientos empresariales diferentes. El primero fue un emprendimiento de ventas de suministros de Arduino, robótica y kits educativos. El segundo se centró en brindar educación en robótica a estudiantes en escuelas, y el tercero fue una plataforma de educación en línea que brindó capacitación en electrónica y programación a adultos.',
+  },
+  "myjourney-text-3": {
+    en: '<span class="resaltar">- 2019 -</span> Through these ventures, I learned a great deal about project management, leading a team, understanding business metrics, communication, leadership, fundraising, marketing, sales, and customer service.',
+    es: '<span class="resaltar">- 2019 -</span> A través de estos emprendimientos, aprendí mucho sobre gestión de proyectos, liderazgo de equipos, comprensión de métricas empresariales, comunicación, liderazgo, recaudación de fondos, marketing, ventas y servicio al cliente.',
+  },
+  "myjourney-text-4": {
+    en: '<span class="resaltar">- 2022 -</span> In the past year, I\'ve been humbly repositioning myself to learn more about data, which was one of my weaknesses in my previous entrepreneurial ventures.',
+    es: '<span class="resaltar">- 2022 -</span> En el último año, me he estado reposicionando humildemente para aprender más sobre los datos, lo cual era una de mis debilidades en mis emprendimientos empresariales anteriores.',
+  },
+  "myjourney-text-5": {
+    en: "I took a <span class='resaltar'>Google data analytics</span>  course, where I learned to program in R, Python, and various BI tools to expedite the visualization, reporting, and decision-making processes based on data. I am currently studying Business Analytics at Coder House and mentoring three entrepreneurial ventures. I am excited to apply the knowledge I've gained and collaborate with other companies and startups to help them grow with the insights that I can provide.",
+    es: "Tomé un curso de <span class='resaltar'>análisis de datos de Google</span>, donde aprendí a programar en R, Python y varias herramientas de BI para agilizar la visualización, presentación de informes y procesos de toma de decisiones basados en datos. Actualmente, estoy estudiando Análisis de Negocios en Coder House y mentorizando tres emprendimientos empresariales. Estoy emocionado de aplicar los conocimientos que he adquirido y colaborar con otras empresas y startups para ayudarlos a crecer con los conocimientos que puedo proporcionar.",
+  },
+  "skills-title": {
+    en: "<i class='bx bx-bulb bx-md'></i> Soft and Tech Skills",
+    es: "<i class='bx bx-bulb bx-md'></i> Habilidades Blandas y Técnicas.",
+  },
+  "soft-skills": {
+    en: "Soft",
+    es: "Blandas",
+  },
+  "tech-skills": {
+    en: "Tech",
+    es: "Técnicas",
+  },
+  "projects-title": {
+    en: "<i class='bx bx-briefcase bx-md'></i> My Projects",
+    es: "<i class='bx bx-briefcase bx-md'></i> Mis Proyectos",
+  },
+  "project-google-final-title": {
+    en: "Google Data Analytics Final Project",
+    es: "Proyecto Final de Análisis de Datos de Google.",
+  },
+  "project-google-final-text": {
+    en: 'For my final project, I analyzed data for Cyclistic, a Chicago-based bike sharing company. Using the "R" programming language, I completed the Google Data Analysis Professional Certification.',
+    es: 'Para mi proyecto final, analicé datos para Cyclistic, una empresa de bicicletas compartidas con sede en Chicago. Usando el lenguaje de programación "R", completé la Certificación Profesional de Análisis de Datos de Google.',
+  },
+  "project-google-final-btn": {
+    en: "Project Repository",
+    es: "Repositorio del Proyecto",
+  },
+  "project-people-dashboard-title": {
+    en: "Dashboard for the people's Department",
+    es: "Tablero para el Departamento de la Gente.",
+  },
+  "project-people-dashboard-text": {
+    en: "The human resources team of a company asked me to build a Dashboard for them so they can analyze all the data they have separated in 3 DB taking into account the fields of: number of employees, each department, salaries, average evaluation, gender and location on the US map.",
+    es: "El equipo de recursos humanos de una empresa me pidió que construyera un Tablero para ellos para que puedan analizar todos los datos que tienen separados en 3 bases de datos, teniendo en cuenta los campos de: número de empleados, cada departamento, salarios, evaluación promedio, género y ubicación en el mapa de EE. UU.",
+  },
+  "project-people-dashboard-btn": {
+    en: "Try the Dashboard",
+    es: "Probar Tablero",
+  },
+  "connect-title": {
+    en: "<i class='bx bx-conversation bx-md'></i> Connect with me",
+    es: "<i class='bx bx-conversation bx-md'></i> Conéctate conmigo",
+  },
+  "my-courses-btn": {
+    en: '<i class="bx bx-link-external bx-sm social-link-icon"></i>  My Courses',
+    es: '<i class="bx bx-link-external bx-sm social-link-icon"></i>  Mis Cursos',
+  },
+  "": {
+    en: "",
+    es: "",
+  },
+};
--- js/main.js
+++ js/main.js
@@ -1,18 +1,85 @@
-// Dark Light switch
 const switchModeBtn = document.getElementById("cb1");
+const switchLanguageBtn = document.getElementById("switchLang");
 const body = document.body;
 const btnIcon = document.getElementById("nav-button-icon");
+let root = document.documentElement;
+let theme = localStorage.getItem("theme");
+let language = localStorage.getItem("language");
 
-switchModeBtn.addEventListener("click", function() {
-  body.classList.toggle("day-background");
-  if (btnIcon.classList.contains("bx-sun")) {
-    btnIcon.classList.remove("bx-sun");
-    btnIcon.classList.add("bx-moon");
+if (language) {
+  updateText(language);
+  switchLanguageBtn.innerHTML = language === "es" ? "ESP" : "ENG";
+} else {
+  language = getLanguage();
+  updateText(language);
+  switchLanguageBtn.innerHTML = language === "es" ? "ESP" : "ENG";
+}
+
+if (theme) {
+  switchTheme();
+} else {
+  if (window.matchMedia) {
+    // Check if the dark-mode Media-Query matches
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      theme = "dark";
+      switchTheme();
+    } else {
+      theme = "light";
+      switchTheme();
+    }
   } else {
+    theme = "light"; // default to light
+  }
+}
+
+function switchTheme() {
+  if (theme === "dark") {
+    root.style.setProperty("--background-color", "#11161e");
+    root.style.setProperty("--text-color", "#eaebeb");
+    root.style.setProperty(
+      "--navbar-resume-btn-bg",
+      "rgba(255, 255, 255, 0.2)"
+    );
+    root.style.setProperty("--navbar-bg-contrast", "rgba(33, 37, 41, 0.95)");
+
     btnIcon.classList.remove("bx-moon");
     btnIcon.classList.add("bx-sun");
 
+    localStorage.setItem("theme", "dark");
+  } else if (theme === "light") {
+    root.style.setProperty("--background-color", "#eaebeb");
+    root.style.setProperty("--text-color", "#11161e");
+    root.style.setProperty("--navbar-resume-btn-bg", "rgba(20, 0, 0, 0.1)");
+    root.style.setProperty("--navbar-bg-contrast", "rgba(255, 255, 255, 0.95)");
+
+    btnIcon.classList.remove("bx-sun");
+    btnIcon.classList.add("bx-moon");
+
+    localStorage.setItem("theme", "light");
   }
+}
+
+switchLanguageBtn.addEventListener("click", function () {
+  if (language === "es") {
+    language = "en";
+    updateText(language);
+    localStorage.setItem("language", language);
+    switchLanguageBtn.innerHTML = "ENG";
+  } else {
+    language = "es";
+    updateText(language);
+    localStorage.setItem("language", language);
+    switchLanguageBtn.innerHTML = "ESP";
+  }
+});
+
+switchModeBtn.addEventListener("click", function () {
+  if (theme === "dark") {
+    theme = "light";
+  } else if (theme === "light") {
+    theme = "dark";
+  }
+  switchTheme();
 });
 
 // Navbar
@@ -31,3 +98,29 @@ navbarToggle.addEventListener("click", toggleNavbarVisibility);
 navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
 navbarMenu.addEventListener("click", toggleNavbarVisibility);
 
+function getLanguage() {
+  const userLang = navigator.language;
+  const langCode = userLang.split("-")[0]; // extract the language code from the user's preference
+  return langCode;
+}
+
+function updateText(language) {
+  if (language === "en" || language === "es") {
+    const elements = document.querySelectorAll("[lang]"); // find all elements with a "data-lang" attribute
+    elements.forEach((el) => {
+      const id = el.id; // get the ID of the current element
+      if (id !== "html") {
+        try {
+          const text = lang[id][language]; // get the appropriate translation for the current language
+          el.innerHTML = text; // update the text content of the current element
+        } catch (e) {
+          console.log("Error: No key found for: " + id);
+        }
+      }
+      if (id === "nav-btn-resume") {
+        const text = lang["resume-url"][language]; // get the appropriate translation for the current language
+        el.setAttribute("href", text); // update the text content of the current element
+      }
+    });
+  }
+}
